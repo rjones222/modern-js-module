0b7478ec51305f3044721e95e016ba721569cad5
Rick Jones <rjones@spoonflower.com>
2016-08-10 22:34:30 -0400

    removed new tutorial files

--- todo-api/todo.js
+++
@@ -1,5 +0,0 @@
-var todoCrud = require('./lib/crud.js');
-
-module.exports = {
-  data: todoCrud
-};
--- todo-web/webpack.config.js
+++
@@ -1,9 +0,0 @@
-var conf = {
-  entry: './browser.js',
-  output: {
-    path: './public/js/',
-    filename: 'bundle.js'
-  }
-};
-
-module.exports = conf;
